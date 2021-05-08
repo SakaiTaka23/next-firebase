d43b8f07cd5d62687d66d96ab43da1b6f54167f1
SakaiTaka23 <tt3.sakai@gmail.com>
2021-05-08 14:25:09 +0900

    handlerでjsonを返すように変更

--- backend/handler/authHandler.go
+++ backend/handler/authHandler.go
@@ -3,9 +3,13 @@ package handler
 import "github.com/gofiber/fiber/v2"
 
 func Public(c *fiber.Ctx) error {
-	return c.SendString("hello public")
+	return c.JSON(fiber.Map{
+		"message": "hello public",
+	})
 }
 
 func Private(c *fiber.Ctx) error {
-	return c.SendString("hello private")
+	return c.JSON(fiber.Map{
+		"message": "hello private",
+	})
 }
