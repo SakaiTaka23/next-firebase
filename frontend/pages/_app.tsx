357422448ab6c19f50ac7c39737481ddda9bcaa1
SakaiTaka23 <tt3.sakai@gmail.com>
2021-05-08 16:48:20 +0900

    ロード初回でトークンを取得するエフェクト実装

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,13 +2,21 @@ import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import AuthContext from '../src/lib/AuthContext';
 import authReducer from '../src/lib/authReducer';
-import { listenAuthState } from '../src/lib/firebase';
+import { Firebase, firebaseUser, listenAuthState } from '../src/lib/firebase';
 import { useEffect, useReducer } from 'react';
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [state, dispatch] = useReducer(authReducer.reducer, authReducer.initialState);
   useEffect(() => {
     return listenAuthState(dispatch);
+  }, []);
+
+  useEffect(() => {
+    const setToken = async () => {
+      const token = await firebaseUser()?.getIdToken();
+      localStorage.setItem('token', token);
+    };
+    setToken();
   }, []);
 
   return (
